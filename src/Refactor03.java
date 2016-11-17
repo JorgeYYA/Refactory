70119d00f80cb9ba404c19e1070c9a5ab1e5e11e
Damir Galimov <drenatovich@gmail.com>
2016-11-17 19:54:17 +0100

    Mensaje

--- src/Refactor03.java
+++ src/Refactor03.java
@@ -1,23 +1,31 @@
 
 public class Refactor03 {
 
+	private static int colPeonNegra;
+	private static int filPeonNegra;
+	private static int colPeonBlanca;
+	private static int filPeonBlanca;
+	private static int sigmovColPeonNegra;
+	private static int sigmovFilPeonNegra;
+	
+	public Refactor03(){
+		colPeonNegra=1;
+		filPeonNegra=3;
+		colPeonBlanca=1;
+		filPeonBlanca=3;
+	}
 	public static void main(String[] args) {
 		//Calculamos el movimiento de un peón en un tablero de ajedrez
 		//Para una partida con más peones
 		//Juegan negras en la parte inferior del tablero
-		int colPeonNegra=1;
-		int filPeonNegra=3;
-		int colPeonBlanca=1;
-		int filPeonBlanca=3;
-		int sigmovColPeonNegra;
-		int sigmovFilPeonNegra;
+		
 		
 		//Calculamos siguiente movimieto
-		if((colPeonNegra++==colPeonBlanca)&&(filPeonNegra++==filPeonBlanca)){
+		if(comparaMovimientoMasMas()){
 			//Me como blanca colocado en diagonal derecha
 			sigmovColPeonNegra=colPeonNegra++;
 			sigmovColPeonNegra=filPeonNegra++;
-		}else if((colPeonNegra++==colPeonBlanca)&&(filPeonNegra--==filPeonBlanca)){
+		}else if(comparaMovimientoMasMenos()){
 			//Este movimiento está mal
 			//Me como blanca colocado en diagonal izda
 			sigmovColPeonNegra=colPeonNegra++;
@@ -27,5 +35,27 @@ public class Refactor03 {
 			sigmovColPeonNegra=colPeonNegra++;
 		}
 	}
-
+	
+	public static boolean comparaMovimientoMasMas()
+	{
+		boolean resultado;
+		if((colPeonNegra++==colPeonBlanca)&&(filPeonNegra++==filPeonBlanca)){
+			resultado=true;
+		}
+		else{
+			resultado=false;
+		}
+		return resultado;
+	}
+	public static boolean comparaMovimientoMasMenos()
+	{
+		boolean resultado;
+		if((colPeonNegra++==colPeonBlanca)&&(filPeonNegra--==filPeonBlanca)){
+			resultado=true;
+		}
+		else{
+			resultado=false;
+		}
+		return resultado;
+	}
 }
